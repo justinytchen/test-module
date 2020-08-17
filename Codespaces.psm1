b534d101e14cf46d05b08bd2d6df466d42e2d221
Justin Chen <t-chenju@microsoft.com>
2020-08-17 15:26:52 -0700

    refactor/polish

--- Codespaces.psm1
+++ Codespaces.psm1
@@ -3,28 +3,27 @@ function Start-Codespaces {
         [Parameter(Position=0, Mandatory)]
         [string]$Subscription,
         [Parameter(Position=1, Mandatory)]
-        [string]$Plan,
+        [string]$ResourceGroup,
         [Parameter(Position=2, Mandatory)]
+        [string]$Plan,
+        [Parameter(Position=3, Mandatory)]
         [string]$ArmToken,
         [switch]$NoWait
-
     )
 
-    Install-Codespaces
+    $curLoc = Get-Location
+    Install-Codespaces $curLoc "codespacesBin"
 
-    Write-Host "Setting arm token"
     $env:VSCS_ARM_TOKEN=$ArmToken
-
-    Write-Host "Starting codespaces"
 
     $csJob = Start-Job -ScriptBlock {
         $subscription = $using:Subscription
         $plan = $using:Plan
-        "n`n1`n`n" | ./bin/codespaces start -s $subscription -p $plan
+        $resourceGroup = $using:ResourceGroup
+        "n`n`n" | ./bin/codespaces start -s $subscription -p $plan -r $resourceGroup
         $env:VSCS_ARM_TOKEN=""
     }
 
-    $output = ""
     while ($true) {
         $output = Receive-Job $csJob
         if($output.length -gt 0){
@@ -39,17 +38,8 @@ function Start-Codespaces {
         }
     }
 
-    Write-Host "Exiting module"
-
-
-    Write-Host $pid
     if (-not $NoWait) {
-        while (-not (get-runspace -id 1).debugger.IsActive) {
-
-            Write-Host $output
-            sleep 3
-        };
-
+        Write-Host "Waiting for debugger to attach"
         while (-not (get-runspace -id 1).debugger.IsActive) {
 
             Write-Host $output
@@ -59,24 +49,53 @@ function Start-Codespaces {
 }
 
 function Install-Codespaces{
+    param(
+        [Parameter(Position=0, Mandatory)]
+        [string]$BinParentDir,
+        [Parameter(Position=1, Mandatory)]
+        [string]$BinFolderName
+    )
+
     $global:ProgressPreference = "SilentlyContinue"
     Set-StrictMode -Version Latest
     $ErrorActionPreference = "Stop"
     $PSDefaultParameterValues['*:ErrorAction'] = 'Stop'
 
-    New-Item -Path . -Name "bin" -ItemType "directory"
+    if ($null -ne (Get-Process -Name "vsls-agent" -ea "SilentlyContinue")){
+        Write-Host "Ending vsls-agent that was still active from a previous session"
+        Stop-Process -Name "vsls-agent"
+    }
 
-    $destination="./bin"
+    if(Test-Path $BinFolderName){
+        Write-Host "$BinFolderName folder already exists. Deleting and reinstalling."
+        Remove-Item $BinFolderName -force -recurse
+    }
+
+    New-Item -Path $BinParentDir -Name $BinFolderName -ItemType "directory"
+
+    $destination = Join-Path $BinParentDir $BinFolderName
     $tempdestination = New-TemporaryFile
     $webClient = New-Object System.Net.WebClient
     switch ($true) {
+        ($PSVersionTable.PSVersion.Major -lt 6) {
+            # Must be PowerShell Core on Windows
+            Import-Module -Name "Microsoft.PowerShell.Archive"
+            $source = "https://vsoagentdownloads.blob.core.windows.net/vsoagent/VSOAgent_win_3934786.zip"
+
+            Write-Host "Downloading zip file (Windows)"
+            $WebClient.DownloadFile($source, $tempdestination)
+            Write-Host "Extracting from zip file"
+
+            Expand-Archive -Path $tempdestination -Destination $destination -Force
+            break
+        }
         $IsMacOS {
             Import-Module -Name "Microsoft.PowerShell.Archive"
             $source = "https://vsoagentdownloads.blob.core.windows.net/vsoagent/VSOAgent_osx_3920504.zip";
 
-            Write-Host "Downloading"
+            Write-Host "Downloading zip file (MacOS)"
             $WebClient.DownloadFile($source, $tempdestination)
-            Write-Host "Expanding"
+            Write-Host "Extracting from zip file"
 
             Expand-Archive -Path $tempdestination -Destination $destination -Force
             chmod -R +x ./bin
@@ -84,9 +103,9 @@ function Install-Codespaces{
         }
         $IsLinux {
             $source = "https://vsoagentdownloads.blob.core.windows.net/vsoagent/VSOAgent_linux_3929085.tar.gz"
-            Write-Host "Downloading"
+            Write-Host "Downloading tar.gz file (Linux)"
             $WebClient.DownloadFile($source, $tempdestination)
-            Write-Host "Expanding"
+            Write-Host "Extracting from tar.gz file"
             tar -xf $tempdestination -C $destination
             break
         }
@@ -95,14 +114,15 @@ function Install-Codespaces{
             Import-Module -Name "Microsoft.PowerShell.Archive"
             $source = "https://vsoagentdownloads.blob.core.windows.net/vsoagent/VSOAgent_win_3934786.zip"
 
-            Write-Host "Downloading"
+            Write-Host "Downloading zip file (Windows)"
             $WebClient.DownloadFile($source, $tempdestination)
-            Write-Host "Expanding"
+            Write-Host "Extracting from zip file"
 
             Expand-Archive -Path $tempdestination -Destination $destination -Force
             break
         }
     }
+
     Remove-Item $tempdestination
-    Write-Host "Done"
+    Write-Host "Done installing codespaces"
 }
