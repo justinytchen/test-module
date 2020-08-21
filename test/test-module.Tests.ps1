9cda6ff0cf97d5e0723da25adadf0404aa97f754
Justin Chen <t-chenju@microsoft.com>
2020-08-21 10:09:42 -0700

    modified test

--- test/test-module.Tests.ps1
+++ test/test-module.Tests.ps1
@@ -5,11 +5,7 @@ Describe 'Module Manifest Tests' {
     It 'Passes Test-ModuleManifest' {
         Write-Host "In first test"
         Write-Host $pid
-        while (-not (get-runspace -id 1).debugger.IsActive) {
-            $p = $pid
-            sleep 1
-        };
         Test-ModuleManifest -Path $ModuleManifestPath | Should Not BeNullOrEmpty
-        $? | Should Be $true
+        $? | Should Be $false
     }
 }
