865a3a3144c64cb879db6fa7ea4dfe1341a51bf7
Justin Chen <t-chenju@microsoft.com>
2020-08-21 17:29:32 -0700

    move breakall

--- codespaces.psm1
+++ codespaces.psm1
@@ -52,9 +52,8 @@ function Start-Codespaces {
 
     if ($Wait) {
         Write-Host "$(Get-TimeStamp) Waiting for debugger to attach"
-        Enable-RunspaceDebug -BreakAll
         while (-not $host.Runspace.debugger.IsActive) {
-
+            Enable-RunspaceDebug -BreakAll
             Write-Host "$(Get-TimeStamp) pid: $pid, Connect: $url"
             Start-Sleep 3
         };
