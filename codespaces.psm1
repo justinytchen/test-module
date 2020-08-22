b2cb3359794b81a3d1b12f8b311eecc7b39f8842
Justin Chen <t-chenju@microsoft.com>
2020-08-21 17:27:42 -0700

    change wait condition

--- codespaces.psm1
+++ codespaces.psm1
@@ -53,7 +53,7 @@ function Start-Codespaces {
     if ($Wait) {
         Write-Host "$(Get-TimeStamp) Waiting for debugger to attach"
         Enable-RunspaceDebug -BreakAll
-        while (-not (get-runspace -id 1).debugger.IsActive) {
+        while (-not $host.Runspace.debugger.IsActive) {
 
             Write-Host "$(Get-TimeStamp) pid: $pid, Connect: $url"
             Start-Sleep 3
