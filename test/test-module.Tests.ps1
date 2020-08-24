83087c8bdedc01ba49c8ca200ba881afc9e2338d
Justin Chen <t-chenju@microsoft.com>
2020-08-24 15:10:29 -0700

    fail test

--- test/test-module.Tests.ps1
+++ test/test-module.Tests.ps1
@@ -4,6 +4,6 @@ $ModuleManifestPath = "$PSScriptRoot\..\$ModuleManifestName"
 Describe 'Module Manifest Tests' {
     It 'Passes Test-ModuleManifest' {
         $sum = 1 + 1
-        $sum | Should -Be 2
+        $sum | Should -Be 3
     }
 }
