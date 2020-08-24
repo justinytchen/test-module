43845345ec2f185db5558bb3828ca978a010d9ff
Justin Chen <t-chenju@microsoft.com>
2020-08-24 12:32:03 -0700

    added dash back

--- test/test-module.Tests.ps1
+++ test/test-module.Tests.ps1
@@ -4,6 +4,6 @@ $ModuleManifestPath = "$PSScriptRoot\..\$ModuleManifestName"
 Describe 'Module Manifest Tests' {
     It 'Passes Test-ModuleManifest' {
         $res = 1 + 1
-        $res | Should Be 3
+        $res | Should -Be 3
     }
 }
